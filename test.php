7bd58ef00bad6dae98ee14214a97dec96555732b
Phattarayutt    Phooprae <34184588+Phattarayutt@users.noreply.github.com>
2019-07-10 09:55:16 +0700

    Update test.php

--- test.php
+++ test.php
@@ -1,9 +1,12 @@
 <?php
+
 $access_token = '2ba07631f6ce1627dfbcbffd70b6fa67';
  
 $url = 'https://api.line.me/bot/pnp/push/verified';
-
-$headers = array('Authorization: Bearer ' . $access_token);
+ 
+$arrayHeader = array();
+$arrayHeader[] = "Content-Type: application/json";
+$arrayHeader[] = "Authorization: Bearer {$access_token}";
 
 $arrayPostData['to'] = "0859556678";
 $arrayPostData['messages'][0]['type'] = "text";
@@ -11,7 +14,8 @@ $arrayPostData['messages'][0]['text'] = "Hello World.";
 
 $ch = curl_init($url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $arrayHeader);
+curl_setopt($ch, CURLOPT_POST, true);
 curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
 curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($arrayPostData));
 $result = curl_exec($ch);
