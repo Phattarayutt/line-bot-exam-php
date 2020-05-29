128c35a23ded608cc0830ccb09852ed1c4e9dfae
Phattarayutt    Phooprae <34184588+Phattarayutt@users.noreply.github.com>
2020-05-29 09:47:23 +0700

    Update profile.php

--- profile.php
+++ profile.php
@@ -1,10 +1,10 @@
 <?php
 
-echo 'update';
+ 
 $access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
 
  
- $userId = 'Ue1f2ed1f1753a2d0e39ef4ad61e2e6b0';
+$userId = 'Ue1f2ed1f1753a2d0e39ef4ad61e2e6b0';
 
 $url = 'https://api.line.me/v2/bot/profile/'.$userId;
 
@@ -17,5 +17,5 @@ curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
 $result = curl_exec($ch);
 curl_close($ch);
 
-echo $result;
+echo "<h6>result</h6>" . $result;
 
